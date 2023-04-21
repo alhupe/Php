486be4ba0f351188d396cb69b3ec7e086d1505ac
Alvaro Huamani Pereira <alhupe@floridauniversitaria.es>
2023-04-21 17:09:12 +0200

    CI

--- Test/calculadoratest.php
+++ Test/calculadoratest.php
@@ -5,7 +5,7 @@ final class CalculadoraTest extends TestCase
 {
 public function testSuma()
 {
-$calc = new Calculadora(3,4);
+$calc = new Calculadora(3,5);
 $this->assertEquals(8, $calc->suma());
 }
 }
